00ab5d152681056a785525ac60ed8d03a3d5d081
skullxcode <pranavthawait@gmail.com>
2026-05-30 22:48:46 +0530

    logos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,6 @@ import WhatIDo from "../Accordian/WhatIDo";
 import "./Home.css";
 
 function Home() {
-
   return (
     <div className="outerhome">
       <section id="home">
@@ -16,9 +15,15 @@ function Home() {
             <h4>Hi, I'm Pranav Thawait</h4>
             <h2>Software Developer</h2>
             <div id="links">
-              <img src="https://img.icons8.com/?size=100&id=3tC9EQumUAuq&format=png&color=737373" />
-              <img src="https://img.icons8.com/?size=100&id=8808&format=png&color=737373" />
-              <img src="https://img.icons8.com/?size=100&id=Y2GfpkgYNp42&format=png&color=737373" />
+              <a href="https://github.com/Pranav00076" target="_blank">
+                <img src="https://img.icons8.com/?size=100&id=3tC9EQumUAuq&format=png&color=737373" />
+              </a>
+              <a href="https://www.linkedin.com/in/pranav-thawait-140a092b2/" target="_blank">
+                <img src="https://img.icons8.com/?size=100&id=8808&format=png&color=737373" />
+              </a>
+              <a href="https://mail.google.com/" target="_blank">
+                <img src="https://img.icons8.com/?size=100&id=Y2GfpkgYNp42&format=png&color=737373" />
+              </a>
             </div>
           </div>
           <div className="right">
@@ -44,10 +49,10 @@ function Home() {
       </section>
       <InfiniteSliding />
       <div id="content">
-            <WhatIDo />
-            <div className="glitch">
-                <LetterGlitch />
-            </div>
+        <WhatIDo />
+        <div className="glitch">
+          <LetterGlitch />
+        </div>
       </div>
     </div>
   );
